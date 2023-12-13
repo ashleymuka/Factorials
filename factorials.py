628ba316ef71382dc2fc35de66a7f6bf38d84e67
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 23:26:42 -0600

    Update factorials.py

--- factorials.py
+++ factorials.py
@@ -1,15 +1,9 @@
 '''
-Author: Ashley Muka
-Assignment Title: Factorials
-Assignment Description: Create a program to calculate the
+Description: Create a program to calculate the
 factorial of a nonnegative integer using recursion
-Due Date:11/03/2023
-Date Created:10/30/2023
-Date Last Modified:11/02/2023
 
 '''
 
-#process
 def factorial_recursive(n):
     if n == 1:
         return n
@@ -20,14 +14,14 @@ def factorial_recursive(n):
 if __name__ == "__main__":
 
     try:
-#input      
+
         num = int(input("Enter a nonnegative number: \n"))
 
         while num < 0:
             num = int(input("Please enter a nonnegative number. \n"))
 
         else:
-#output            
+       
             fact = factorial_recursive(num)
             print(f'The factorial of {num} is {fact}')
 
